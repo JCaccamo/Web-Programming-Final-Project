93ccc9eca55b3f25e5426fd1cf87565e1ed1e26b
JCaccamo <caccamojason@gmail.com>
2022-10-17 23:07:39 -0400

    Updated JournalView.vue and added styling

--- client/src/views/JournalView.vue
+++ client/src/views/JournalView.vue
@@ -9,7 +9,6 @@
             const workoutName = ref("");
             const workoutType = ref("select-workout");
             const exercises = ref([1]);
-            const statusMsg = ref(null);
             const errorMsg = ref(null);
             // Add exercise
             const addExercise = () => {
@@ -37,7 +36,7 @@
                     exercises.value = exercises.value.filter((exercise) => exercise.id !== id);
                     return;
                 }
-                errorMsg.value = "Error: Cannot remove, need to have at least one exercise";
+                errorMsg.value = "Error: You must have at least one exercise";
                 setTimeout(() => {
                     errorMsg.value = false;
                 }, 5000);
@@ -48,7 +47,7 @@
                 addExercise()
             }
             // Create workout
-            return { workoutName, workoutType, exercises, statusMsg, errorMsg, addExercise, deleteExercise, workoutChange };
+            return { workoutName, workoutType, exercises, errorMsg, addExercise, deleteExercise, workoutChange };
         }
     }
 </script>
@@ -58,93 +57,174 @@
         <div class="container">
             <h1 class="title">Journal</h1>
             <h2 class="subtitle">Page for adding/editing workouts</h2>
-            <!-- Status Message-->
-            <div v-if="statusMsg || errorMsg">
-                <p>{{statusMsg}}</p>
-                <p>{{errorMsg}}</p>
+            <!-- Error Message-->
+            <div v-if="errorMsg">
+                <p class="notification is-danger is-light mb-5">{{errorMsg}}</p>
             </div>
-            <!-- Create -->
-            <div>
-                <!-- Form -->
-                <form>
-                    <h1>Record workout</h1>
-                    <!-- Workout Name -->
-                    <div>
-                        <label for="workout-name">Workout Name</label>
-                        <input type="text" required id="workout-name" v-model="workoutName">
+            <!-- Form -->
+            <form>
+                <h1 class="title is-5">Record workout</h1>
+                <div class="columns">
+                    <div class="column">
+                        <!-- Workout Name -->
+                        <div class="field">
+                            <label class="label" for="workout-name">Workout Name</label>
+                            <div class="control">
+                                <input class="input" type="text" required id="workout-name" v-model="workoutName">
+                            </div>
+                        </div>
                     </div>
-                    <!-- Workout Type -->
-                    <div>
-                        <label for="workout-type">Workout Type</label>
-                        <select id="workout-type" required @change="workoutChange" v-model="workoutType">
-                            <option value="select-workout">Select Workout</option>
-                            <option value="strength">Strength Training</option>
-                            <option value="cardio">Cardio</option>
-                        </select>
+                    <div class="column">
+                        <!-- Workout Type -->
+                        <div class="field">
+                            <label class="label" for="workout-type">Workout Type</label>
+                            <div class="control">
+                                <div class="select is-fullwidth">
+                                    <select id="workout-type" required @change="workoutChange" v-model="workoutType">
+                                        <option value="select-workout">Select Workout</option>
+                                        <option value="strength">Strength Training</option>
+                                        <option value="cardio">Cardio</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <!-- Strength Training Inputs -->
-                    <div v-if="workoutType === 'strength'">
-                        <div v-for="(item, index) in exercises" :key="index">
-                            <div>
-                                <label for="exercise-name">Exercise</label>
-                                <input required type="text" v-model="item.exercise">
+                </div><!-- end of columns -->
+                <!-- Strength Training Inputs -->
+                <div v-if="workoutType === 'strength'">
+                    <div v-for="(item, index) in exercises" :key="index">
+                        <div class="columns">
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="exercise-name">Exercise</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.exercise">
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <label for="sets">Sets</label>
-                                <input required type="text" v-model="item.sets">
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="sets">Sets</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.sets">
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <label for="reps">Reps</label>
-                                <input required type="text" v-model="item.reps">
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="reps">Reps</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.reps">
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <label for="weight">Weight (lbs)</label>
-                                <input required type="text" v-model="item.weight">
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="weight">Weight (lbs)</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.weight">
+                                    </div>
+                                </div>
                             </div>
-                                <img src="@/assets/trash-icon.png" alt="" @click="deleteExercise(item.id)">
+                        </div><!-- end of columns -->
+                        <div class="columns mb-5">
+                            <div class="column is-one-quarter">
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-info is-light" type="button" @click="addExercise">
+                                            Add Exercise
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
-                        <button type="button" @click="addExercise">
-                            Add Exercise
+                            <div class="column is-one-quarter">
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-danger is-light" type="button" @click="deleteExercise(item.id)">
+                                            Delete Exercise
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- end of columns -->
+                    </div><!-- end of v-for -->
+                </div><!-- end of v-if -->
+                <!-- Cardio Inputs -->
+                <div v-if="workoutType === 'cardio'">
+                    <div v-for="(item, index) in exercises" :key="index">
+                        <div class="columns">
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="cardio-type">Type</label>
+                                    <div class="select is-fullwidth">
+                                        <select id="cardio-type" v-model="item.cardioType">
+                                            <option value="run">Run</option>
+                                            <option value="walk">Walk</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="distance">Distance (mi)</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.distance">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="duration">Duration (min)</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.duration">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <div class="field">
+                                    <label class="label" for="pace">Pace (mph)</label>
+                                    <div class="control">
+                                        <input class="input" required type="text" v-model="item.pace">
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- end of columns -->
+                        <div class="columns mb-5">
+                            <div class="column is-one-quarter">
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-info is-light" type="button" @click="addExercise">
+                                            Add Exercise
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="column is-one-quarter">
+                                <div class="field">
+                                    <div class="control">
+                                        <button class="button is-danger is-light" type="button" @click="deleteExercise(item.id)">
+                                            Delete Exercise
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- end of columns -->
+                    </div><!-- end of v-for -->
+                </div><!-- end of v-if -->
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-info" type="submit">
+                            Record Workout
                         </button>
                     </div>
-                    <!-- Cardio Inputs -->
-                    <div v-if="workoutType === 'cardio'">
-                        <div v-for="(item, index) in exercises" :key="index">
-                            <div>
-                                <label for="cardio-type">Type</label>
-                                <select id="cardio-type" v-model="item.cardioType">
-                                    <option value="#">Select Type</option>
-                                    <option value="run">Run</option>
-                                    <option value="walk">Walk</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label for="distance">Distance</label>
-                                <input required type="text" v-model="item.distance">
-                            </div>
-                            <div>
-                                <label for="duration">Duration</label>
-                                <input required type="text" v-model="item.duration">
-                            </div>
-                            <div>
-                                <label for="pace">Pace (mph)</label>
-                                <input required type="text" v-model="item.pace">
-                            </div>
-                                <img src="@/assets/trash-icon.png" alt="" @click="deleteExercise(item.id)">
-                            </div>
-                            <button type="button" @click="addExercise">
-                            Add Exercise
-                            </button>
-                        </div>
-                    <button type="submit">
-                        Record Workout
-                    </button>
-                </form><!-- end of form -->
-            </div><!-- end of create -->
-        </div><!-- end of container div -->
-    </div><!-- end of section div -->
+                </div>
+            </form>
+        </div><!-- end of container -->
+    </div><!-- end of section -->
 </template>
 
 <style lang="scss">
-
+    img:hover {
+        cursor: pointer;
+    }
 </style>
