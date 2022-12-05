503dbac67d4bec3d933e1bcf94c5456bffc2368d
JCaccamo <caccamojason@gmail.com>
2022-12-05 13:30:43 -0500

    Installed mongodb, dotenv, updated users.json

--- server/models/users.js
+++ server/models/users.js
@@ -1,11 +1,11 @@
-const data = require('../data/data.json');
+const data = require('../data/users.json');
 
 function getUsers() {
     return data.users;
 }
 
 function getUser(id) {
-    return data.users.find(p => p.userName === userName);
+    return data.users.find(p => p.id === id);
 }
 
 module.exports = {
